737ac753ece42474a1eb0255d5d91e1a293cfd21
Vinicius Henrique <callamktdigital@gmail.com>
2026-02-12 12:11:52 -0300

    delete

--- src/utils/enable_realtime.sql
+++
@@ -1,5 +0,0 @@
--- Enable Realtime for communication_recipients
-alter publication supabase_realtime add table communication_recipients;
-
--- Enable Realtime for enrollments (for document alerts)
-alter publication supabase_realtime add table enrollments;
